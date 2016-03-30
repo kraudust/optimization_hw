3d02100c429e64e06c4c35a30b5cb9065c602039
kraudust <dustan.kraus@gmail.com>
2016-03-30 16:25:42 -0600

    woot

--- HW_6/problem6_2.py
+++ HW_6/problem6_2.py
@@ -19,27 +19,32 @@ def func_b(x):
 	return f, c
 	
 if __name__ == '__main__':
-	x1 = 0.
-	x2 = 0.
-	x3 = 0.
-	lb = -1000000.
-	ub = 1000000.
-	x_orig = np.array([x1, x2, x3])
-	optimizer = SNOPT()
-	xopt, fopt, info = optimize(func, x_orig, lb, ub, optimizer)
-	
-	print "---------------Results Part (a)----------------------"
-	print "Original X: ", x_orig
-	print "Orig Func Val: ", func(x_orig)
-	print "Optimal X: ", xopt
-	print "Opt Func Val:, ", fopt
-	
-	xopt, fopt, info = optimize(func_b, x_orig, lb, ub, optimizer)
-	
-	print "---------------Results Part (b)----------------------"
-	print "Original X: ", x_orig
-	print "Orig Func Val: ", func(x_orig)
-	print "Optimal X: ", xopt
-	print "Opt Func Val:, ", fopt
+    x1 = 0.
+    x2 = 0.
+    x3 = 0.
+    lb = -1000000.
+    ub = 1000000.
+    x_orig = np.array([x1, x2, x3])
+    optimizer = SNOPT()
+    ##################### Part (a) #########################################
+    xopt, fopt, info = optimize(func, x_orig, lb, ub, optimizer)
+
+    print '\n', "---------------Results Part (a) Deterministic----------------------"
+    print "Original X: ", x_orig
+    print "Orig Func Val: ", func(x_orig)[0]
+    print "Optimal X: ", xopt
+    print "Opt Func Val: ", fopt, '\n'
+
+    xopt_b, fopt_b, info = optimize(func_b, xopt, lb, ub, optimizer)
+
+    ##################### Part (b) #########################################	
+    print "-----------Results Part (b) Worst Case Tolerances------------------"
+    print "Original X: ", xopt
+    print "Orig Func Val: ", fopt
+    print "Optimal X: ", xopt_b
+    print "Opt Func Val: ", fopt_b, '\n'
+
+    ##################### Part (c) #########################################
+    #sig_x1 = 0.033
 	
 	
